2dcf59ed220b233ae513c3d3d0ef11eea17d748d
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:25:47 +0300

    add constants

---
+++ src/WebApp.Business/Constants/ResponseMessage.cs
@@ -0,0 +1,11 @@
+namespace WebApp.Business.Constants
+{
+    public static class ResponseMessage
+    {
+        public const string ServerError = "Something went wrong. Please try again later.";
+        public const string InvalidRequest = "Invalid Request";
+        public const string SampleNotFound = "Sample Not Found";
+        public const string AddedSuccessfully = "Successfully Added";
+        public const string AddedFailed = "Added Failed";
+    }
+}
---
+++ src/WebApp.Business/Constants/ValidationMessage.cs
@@ -0,0 +1,9 @@
+namespace WebApp.Business.Constants
+{
+    public static class ValidationMessage
+    {
+        public const string InvalidId = "Invalid Id parameter";
+        public const string TitleRequired = "Title parameter is required";
+        public const string DetailRequired = "Detail parameter is required";
+    }
+}
