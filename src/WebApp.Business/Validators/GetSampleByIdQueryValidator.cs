83cd5b49ee6c93bb74af2e220cbc515733a0c609
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:27:16 +0300

    Create GetSampleByIdQueryValidator.cs

---
+++ src/WebApp.Business/Validators/GetSampleByIdQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using WebApp.Business.Constants;
+using WebApp.Business.Queries;
+
+namespace WebApp.Business.Validators
+{
+    public class GetSampleByIdQueryValidator : AbstractValidator<GetSampleByIdQuery>
+    {
+        public GetSampleByIdQueryValidator()
+        {
+            RuleFor(request => request.Id)
+                .GreaterThanOrEqualTo(1)
+                .WithMessage(ValidationMessage.InvalidId);
+        }
+    }
+}
