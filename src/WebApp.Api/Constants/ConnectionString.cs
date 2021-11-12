01d9b4b43b1bbee35ad4050b8cf9485e50f6ec8e
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:32:47 +0300

    update appSettings.json

---
+++ src/WebApp.Api/Constants/ConnectionString.cs
@@ -0,0 +1,8 @@
+namespace WebApp.Api.Constants
+{
+    public static class ConnectionString
+    {
+        public const string ReadonlyDb = nameof(ReadonlyDb);
+        public const string WritableDb = nameof(WritableDb);
+    }
+}
