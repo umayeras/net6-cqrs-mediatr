4202a5515df4c4c9e42d068ffc333e8f6347804c
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:26:37 +0300

    Create ValidationError.cs

---
+++ src/WebApp.Business/Models/ValidationError.cs
@@ -0,0 +1,14 @@
+namespace WebApp.Business.Models
+{
+    public class ValidationError
+    {
+        public string Field { get; set; }
+        public string Message { get; set; }
+
+        public ValidationError(string field, string message)
+        {
+            Field = field;
+            Message = message;
+        }
+    }
+}
