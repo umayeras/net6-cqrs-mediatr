ffcf569fd9283a2abeafd036e293c0f5fbbaedfe
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:26:31 +0300

    Create ValidationErrorResponse.cs

---
+++ src/WebApp.Business/Responses/ValidationErrorResponse.cs
@@ -0,0 +1,22 @@
+using WebApp.Business.Constants;
+using WebApp.Business.Models;
+
+namespace WebApp.Business.Responses
+{
+    public class ValidationErrorResponse : ServiceResponse
+    {
+        public List<ValidationError> Errors { get; set; }
+
+        public ValidationErrorResponse(List<ValidationError> errors)
+        {
+            Success = false;
+            Message = ResponseMessage.InvalidRequest;
+            Errors = errors;
+        }
+
+        public static ValidationErrorResponse Create(List<ValidationError> errors)
+        {
+            return new ValidationErrorResponse(errors);
+        }
+    }
+}
