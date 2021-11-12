d7ed21b003ffbebde54a0e4e3a4a014d9b3eec8f
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:27:02 +0300

    Create GetSamplesHandler.cs

---
+++ src/WebApp.Business/Handlers/GetSamplesHandler.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Business.Queries;
+using WebApp.Business.Responses;
+using WebApp.Data.Entities;
+using WebApp.Data.Repositories.Abstract;
+
+namespace WebApp.Business.Handlers
+{
+    public class GetSamplesHandler : IRequestHandler<GetSamplesQuery, ServiceResponse>
+    {
+        private readonly IReadOnlyRepository<Sample> repository;
+
+        public GetSamplesHandler(IReadOnlyRepository<Sample> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<ServiceResponse> Handle(GetSamplesQuery request, CancellationToken cancellationToken)
+        {
+            var samples = await repository.GetAsync(
+                predicate: null,
+                orderBy: o => o.OrderBy(x => x.Order),
+                tracking: false,
+                includes: i => i.Include(s => s.Status)
+            );
+
+            return ServiceResponse.CreateSuccess(string.Empty, samples);
+        }
+    }
+}
