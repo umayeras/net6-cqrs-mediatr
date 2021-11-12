e621f3c5cb02bdf0fdbf039e9d94279de864a793
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:26:02 +0300

    Create AddSampleCommand.cs

---
+++ src/WebApp.Business/Commands/AddSampleCommand.cs
@@ -0,0 +1,17 @@
+using MediatR;
+using WebApp.Business.Responses;
+
+namespace WebApp.Business.Commands
+{
+    public class AddSampleCommand : IRequest<ServiceResponse>
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+
+        public AddSampleCommand(string title, string detail)
+        {
+            Title = title;
+            Detail = detail;
+        }
+    }
+}
