cf6d34e0a27316f336c7e98b8c02bf60307ba061
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:27:06 +0300

    Create AddSampleHandler.cs

---
+++ src/WebApp.Business/Handlers/AddSampleHandler.cs
@@ -0,0 +1,29 @@
+using MediatR;
+using WebApp.Business.Commands;
+using WebApp.Business.Constants;
+using WebApp.Business.Responses;
+using WebApp.Data.Entities;
+using WebApp.Data.Repositories.Abstract;
+
+namespace WebApp.Business.Handlers
+{
+    public class AddSampleHandler : IRequestHandler<AddSampleCommand, ServiceResponse>
+    {
+        private readonly IWritableRepository<Sample> repository;
+
+        public AddSampleHandler(IWritableRepository<Sample> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<ServiceResponse> Handle(AddSampleCommand request, CancellationToken cancellationToken)
+        {
+            var sample = new Sample(request.Title, request.Detail);
+            var result = await repository.AddAsync(sample);
+
+            return result != null
+                ? ServiceResponse.CreateError(ResponseMessage.AddedFailed)
+                : ServiceResponse.CreateSuccess(ResponseMessage.AddedSuccessfully);
+        }
+    }
+}
