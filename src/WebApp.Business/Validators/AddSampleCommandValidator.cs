3ed2926607aa2a702e1db7aae59078a934c32ebf
Umay Eras <umayeras@hotmail.com>
2021-11-12 12:27:19 +0300

    Create AddSampleCommandValidator.cs

---
+++ src/WebApp.Business/Validators/AddSampleCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using WebApp.Business.Commands;
+using WebApp.Business.Constants;
+
+namespace WebApp.Business.Validators
+{
+    public class AddSampleCommandValidator : AbstractValidator<AddSampleCommand>
+    {
+        public AddSampleCommandValidator()
+        {
+            RuleFor(request => request.Title)
+                .NotEmpty()
+                .WithMessage(ValidationMessage.TitleRequired);
+            
+            RuleFor(request => request.Detail)
+                .NotEmpty()
+                .WithMessage(ValidationMessage.DetailRequired);
+        }
+    }
+}
